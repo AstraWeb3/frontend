a916cd63b3062cf673724db3f01a887d3d6af8c7
Gio <t-giovannyh@Gios-MacBook-Pro.local>
2025-02-02 14:34:14 -0600

    Added logo image

--- astra-frontend/src/components/NavBar/Navbar.tsx
+++ astra-frontend/src/components/NavBar/Navbar.tsx
@@ -1,11 +1,19 @@
 // src/components/Navbar.tsx
 import Link from "next/link";
 import "../NavBar/Navbar.styles.scss";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="nav flex">
-      <h1 className="logo">Astra</h1>
+      <h1 className="logo">
+        <Image
+          src="https://images.scalebranding.com/circle-a-letter-logo-modern-a-logo-565a8e3b-05a4-4025-b400-ec40a1be79ad.jpg"
+          alt="logo"
+          width={50}
+          height={50}
+        />
+      </h1>
       <div className="links flex">
         <Link href="/" className="hover:underline">
           Home
